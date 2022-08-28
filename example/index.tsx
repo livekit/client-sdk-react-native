cfa4c3063f5de9b7d2c135986d75e6b4961b8c99
davidliu <davidliu@deviange.net>
2022-08-29 03:17:09 +0900

    Better logging for example app

--- example/index.tsx
+++ example/index.tsx
@@ -2,6 +2,12 @@ import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 import { registerGlobals } from 'livekit-react-native';
+import { LogLevel, setLogLevel } from 'livekit-client';
+import { setJSExceptionHandler } from 'react-native-exception-handler';
+setJSExceptionHandler((error) => {
+  console.log('error:', error, error.stack);
+}, true);
 
+setLogLevel(LogLevel.debug);
 registerGlobals();
 AppRegistry.registerComponent(appName, () => App);
