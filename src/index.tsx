6e2ed55503f8e1a9354209c64c0dcf3499107802
davidliu <davidliu@deviange.net>
2023-01-24 20:06:02 +0900

    fix: typescript error ignore

--- src/index.tsx
+++ src/index.tsx
@@ -21,9 +21,19 @@ export function registerGlobals() {
 
 function fixBackgroundAndroid() {
   if (Platform.OS === 'android') {
+    // Due to Node.JS/browser type differences, these won't match correctly locally,
+    // but are fine to use on device. Ignoring type issues.
+    // @ts-expect-error
+    // prettier-ignore
     LKTimers.setTimeout = (...args: Parameters<typeof setTimeout>) => AndroidTimer.setTimeout(...args);
+    // @ts-expect-error
+    // prettier-ignore
     LKTimers.setInterval = (...args: Parameters<typeof setInterval>) => AndroidTimer.setInterval(...args);
+    // @ts-expect-error
+    // prettier-ignore
     LKTimers.clearTimeout = (...args: Parameters<typeof clearTimeout>) => AndroidTimer.clearTimeout(...args);
+    // @ts-expect-error
+    // prettier-ignore
     LKTimers.clearInterval = (...args: Parameters<typeof clearInterval>) => AndroidTimer.clearInterval(...args);
   }
 }
