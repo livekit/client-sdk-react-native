df7a5851e70a3b9106650d1b3bca7a6331044da8
davidliu <davidliu@deviange.net>
2023-06-09 00:49:41 +0900

    ci: more build fixes

--- ci/index.js
+++ ci/index.js
@@ -5,7 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import { registerGlobals } from '@livekit/react-native-webrtc';
+import {registerGlobals} from '@livekit/react-native-webrtc';
 
 registerGlobals();
 AppRegistry.registerComponent(appName, () => App);
