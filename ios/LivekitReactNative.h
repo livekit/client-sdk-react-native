89fbb6347a92c9ea02da43265f11df8d54aa2142
davidliu <davidliu@deviange.net>
2024-01-08 21:39:52 +0900

    fix: fix ios RCTBridgeModule type not found error

--- ios/LivekitReactNative.h
+++ ios/LivekitReactNative.h
@@ -5,6 +5,7 @@
 //  Created by David Liu on 9/4/22.
 //  Copyright © 2022 LiveKit. All rights reserved.
 //
+#import <React/RCTBridgeModule.h>
 
 @interface LivekitReactNative : NSObject <RCTBridgeModule>
 
