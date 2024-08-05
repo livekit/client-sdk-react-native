ae5840d4fdc8d6e893580b2e2f99c8556751bdaf
davidliu <davidliu@deviange.net>
2024-08-05 22:47:46 +0900

    example: fix android call service setup

--- example/index.tsx
+++ example/index.tsx
@@ -4,7 +4,7 @@ import { name as appName } from './app.json';
 import { registerGlobals } from '@livekit/react-native';
 import { LogLevel, setLogLevel } from 'livekit-client';
 import { setupErrorLogHandler } from './src/utils/ErrorLogHandler';
-import { setupCallService } from './src/callservice/CallService.ios';
+import { setupCallService } from './src/callservice/CallService';
 
 // Logging
 setupErrorLogHandler();
