7bc0999880452461947b98225fbffad317d5feb1
davidliu <davidliu@deviange.net>
2024-08-05 15:24:33 +0900

    chore: update @livekit/react-native-webrtc to 125.0.2

--- example/ios/LivekitReactNativeExample/AppDelegate.mm
+++ example/ios/LivekitReactNativeExample/AppDelegate.mm
@@ -1,13 +1,20 @@
 #import "AppDelegate.h"
 #import "LivekitReactNative.h"
 #import <React/RCTBundleURLProvider.h>
+#import <WebRTC/WebRTC.h>
+#import "WebRTCModule.h"
+#import "WebRTCModuleOptions.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   [LivekitReactNative setup];
+  WebRTCModuleOptions *options = [WebRTCModuleOptions sharedInstance];
+  // Optional for debugging WebRTC issues.
+  options.loggingSeverity = RTCLoggingSeverityInfo;
   self.moduleName = @"LivekitReactNativeExample";
+  
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
