a5a16cb7becb360e94fb426f3f89b86e667d41e3
davidliu <davidliu@deviange.net>
2025-06-17 18:17:07 +0900

    fix: add crypto.randomUUID and webstream polyfills

--- src/index.tsx
+++ src/index.tsx
@@ -18,6 +18,7 @@ import type { LogLevel, SetLogLevelOptions } from './logger';
 import RNE2EEManager from './e2ee/RNE2EEManager';
 import RNKeyProvider, { type RNKeyProviderOptions } from './e2ee/RNKeyProvider';
 import { setupNativeEvents } from './events/EventEmitter';
+import { ReadableStream, WritableStream } from 'web-streams-polyfill';
 
 /**
  * Registers the required globals needed for LiveKit to work.
@@ -34,6 +35,8 @@ export function registerGlobals() {
   shimArrayAt();
   shimAsyncIterator();
   shimIterator();
+  shimCryptoUuid();
+  shimWebstreams();
   setupNativeEvents();
 }
 
@@ -100,6 +103,33 @@ function shimIterator() {
   var shim = require('well-known-symbols/Symbol.iterator/shim');
   shim();
 }
+
+function shimCryptoUuid() {
+  if (typeof global.crypto?.randomUUID !== 'function') {
+    global.crypto.randomUUID = () =>
+      'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        /* eslint-disable no-bitwise */
+        const r = (Math.random() * 16) | 0;
+        const v = c === 'x' ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      }) as `${string}-${string}-${string}-${string}-${string}`;
+  }
+}
+
+function shimWebstreams() {
+  // @ts-expect-error: global.WritableStream isn't typed here.
+  if (typeof global.WritableStream === 'undefined') {
+    // @ts-expect-error
+    global.WritableStream = WritableStream;
+  }
+
+  // @ts-expect-error: global.ReadableStream isn't typed here.
+  if (typeof global.ReadableStream === 'undefined') {
+    // @ts-expect-error
+    global.WritableStream = ReadableStream;
+  }
+}
+
 export * from './hooks';
 export * from './components/BarVisualizer';
 export * from './components/LiveKitRoom';
